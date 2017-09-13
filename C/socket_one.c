8c59f65fc7f0447ec48d2b32b5328dc1b7981f3b
Jarred Parr <jparr721@gmail.com>
2017-09-13 09:55:53 -0400

    Slow work in progress, schoolwork is killer

--- C/socket_one.c
+++ C/socket_one.c
@@ -15,3 +15,12 @@ struct sockaddr {
 	char 							sa_data[14];	// 14 bytes of protocol address
 }
 
+// Use this for IPv4
+struct sockaddr_in {
+	short int 						sin_family; // Address family, AF_INET
+	unsigned short int 		sin_port; // Port number
+	struct in_addr				sin_addr: // Internet address
+	unsigned char					sin_zero[8] // Same size as struct sockaddr
+}
+
+
