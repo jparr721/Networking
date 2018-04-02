321ba9dca512d1107e69e3f95b1ba7deb88888fe
Jarred Parr <jparr721@gmail.com>
2018-04-01 20:39:56 -0400

    Adding build systems

--- Titan/app/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "../includes/server.hpp"
-
-int main() {
-	Server s;
-	s.server_loop();
-}
---
+++ Titan/server/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/server.hpp"
+
+int main() {
+	Server s;
+	s.server_loop();
+}
